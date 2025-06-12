eef6f39310738baa6a33b6077f107cc424b01bab
wilquersantos <wilquersantos360@gmail.com>
2025-06-11 21:57:30 -0300

    Criação da classe ContaTerminal e realizado a tarefa de solicitar as informações do usuario com sc scanner.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,9 +1,28 @@
-
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o numero da conta: ");
+        var NumeroConta = sc.nextInt();
+        sc.nextLine(); // Consumir a quebra de linha pendente
+        System.out.println("Digite o número da agência: ");
+        String numeroAgencia = sc.nextLine();
+        System.out.println("Digite o nome do cliente: ");
+        String nomeCliente = sc.nextLine();
+        System.out.println("Digite o saldo da conta: ");
+        double saldoConta = sc.nextDouble();
+
+
+        sc.close();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua conta é: " + NumeroConta + " e agência: " + numeroAgencia);
+        System.out.println("Seu saldo é: R$" + saldoConta + ", já está disponível para saque.");
+
+
 
 
     }
